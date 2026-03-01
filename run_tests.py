1bfec7c36c18280f1bf990a894ac16a2ba998b9b
Bereket Girma <145808743+BereketGirma@users.noreply.github.com>
2026-02-28 18:55:23 -0600

    refactor: re-format and lint all files
    What changed?
    - Removed unused imports and readjusted lines to be under 88 characters in one line.
    
    Why this was needed?
    - Now that CI is setup to check linting and formatting, every file needs to be formatted correctly so it doesn't fail.

--- run_tests.py
+++ run_tests.py
@@ -40,7 +40,10 @@ def run_all_tests():
 
 def run_coverage_tests():
     """Run tests with coverage"""
-    command = "python -m pytest apps/ --cov=apps.accounts --cov-report=html --cov-report=term-missing"
+    command = (
+        "python -m pytest apps/ --cov=apps.accounts",
+        "--cov-report=html --cov-report=term-missing",
+    )
     return run_command(command, "Tests with Coverage")
 
 
